8fae545dd46070a978ec1580b9573a4b2b047a18
Rostyslav Fridman <rostyslav.fridman@globallogic.com>
2014-07-10 21:35:42 +0000

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "checkinstall"
 maintainer       "Rostyslav Fridman"
 maintainer_email "rostyslav.fridman@gmail.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures checkinstall"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.0.0"
